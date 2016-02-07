8b0fddf40c31de329b56df81b4bef0369078220f
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 11:20:53 -0800

    Add options size to gravatar_for helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
 	# Returns the Gravatar for the given user.
-	def gravatar_for(user)
-		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)			# create an id using MD5 hash of email address
-		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"	# create gravatar URL
-		image_tag(gravatar_url, alt: user.name, class: "gravatar")			# return image_tag
+	def gravatar_for(user, options = { size: 100 })
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)						# create an id using MD5 hash of email address
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"	# create gravatar URL
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")						# return image_tag
 	end
 end
