51e21975ac1693737d4a6471c031abab939fd79f
leslie kimm <leslie.kimm@gmail.com>
2016-02-06 19:16:34 -0800

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-	# Returns full title on a per=page basis
-	def full_title(page_title = '')
-		base_title = "Ruby on Rails Tutorial Sample App"
-		if page_title.empty?
-			base_title
-		else
-			page_title + " | " + base_title
+	# Returns full title on a per-page basis
+	def full_title(page_title = '')								# Method def, optiona arg
+		base_title = "Ruby on Rails Tutorial Sample App"		# Variable assignment
+		if page_title.empty?									# Boolean test - if no page_title
+			base_title											# Implicit return - return value of base_title
+		else													# Otherwise
+			page_title + " | " + base_title						# Stirng concatenation and return
 		end
 	end
 end
