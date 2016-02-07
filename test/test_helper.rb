6e152605e964914b2d7fd62133ad73369c3c95d8
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 15:30:26 -0800

    Add test helper is_logged_in

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,13 @@ require 'minitest/reporters'		# make tests show in red & green
 Minitest::Reporters.use!			# section 3.7 - advanced testing setup
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  	# Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  	fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  	# Add more helper methods to be used by all tests here...
+
+  	# Returns true if a test user is logged in.
+  	def is_logged_in?
+  		!session[:user_id].nil?
+  	end
 end
