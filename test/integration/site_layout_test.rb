8d0a15a925f93f14b9377d3a875e7834a7ed58b9
leslie kimm <leslie.kimm@gmail.com>
2016-02-06 20:38:42 -0800

    Add integration test to test site HTML layout

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+	test "layout links" do									# test layout links by checking HTML structure of our site
+		get root_path										# get root path (home page)
+		assert_template 'static_pages/home'					# check we're using the right template
+		assert_select "a[href=?]", root_path, count: 2		# check there are two links that go to root path
+		assert_select "a[href=?]", help_path				# check there is one link to help path
+		assert_select "a[href=?]", about_path				# check there is one link to about path
+		assert_select "a[href=?]", contact_path				# check there is one link to contact path
+
+	end
+end
