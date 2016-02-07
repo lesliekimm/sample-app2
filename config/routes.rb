494dcd3e9fd0acaa9d196d939d8486a7a3654771
leslie kimm <leslie.kimm@gmail.com>
2016-02-06 20:42:23 -0800

    Create Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
     root             'static_pages#home'  		# sets root route to /static_pages/home --> carries out home action in static_pages_controller.rb
     # get  'static_pages/about'					# maps requests for URL /static_pages/about to about action in static_pages_controller.rb
     get 'about'   => 'static_pages#about'
