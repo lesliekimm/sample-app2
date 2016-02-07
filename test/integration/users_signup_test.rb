e30d127f7b5325f4caf86058d289fc361dd57fdc
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 10:43:04 -0800

    write integration test for submittig invalid info in signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+	test "invalid signup information" do
+		get signup_path													# get signup path
+		assert_no_difference 'User.count' do							# check that count doesn't change with invalid submit
+			post users_path, user: { name: "",							# post users_path --> calls create action in user controller
+									 email: "user@invalid",				# use invalid attributes for submission
+									 password: "foo",
+									 password_confirmation: "bar" }
+		end
+		assert_template 'users/new'										# check that failed submission re-renders new action
+	end
+end
