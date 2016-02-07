bec7fa2755cc4817cad791dd4b71d8364b213508
leslie kimm <leslie.kimm@gmail.com>
2016-02-06 17:59:49 -0800

    Add minitest-reporters to show test in red/gren

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'		# make tests show in red & green
+Minitest::Reporters.use!			# section 3.7 - advanced testing setup
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
