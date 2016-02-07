076cd7ceb1b17d8f6d83d0f0bb91a2618f553c47
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 13:13:52 -0800

    Add flash message for errors upon login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,19 @@
 class SessionController < ApplicationController
-  def new
-  end
+  	def new
+  	end
+
+  	def create
+  		user = User.find_by(email: params[:session][:email].downcase)
+
+  		if user && user.authenticate(params[:session][:password])		# authenticate method is provided by has_secure_password
+  			# Log the user in and redirect to user's show page
+  		else
+  			# Create an error message and display login page
+  			flash[:danger] = 'Invalid email/password combination'		# not quite right
+  			render 'new'
+  		end
+  	end
+
+  	def destroy
+  	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  	def new
+    def new
   		@user = User.new					# signup route calls this action
   	end
 
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   		@user = User.new(user_params)		# use aux method defined in private to require user and permit only name, email & pws
   		if @user.save
   			flash[:success] = "Welcome to the Sample App!"
-  			redirect_to @user 				# same as redirect_to user_url(@user) - performs show action
+  			redirect_to @user               # same as redirect_to user_url(@user) - performs show action
   		else
   			render 'new'					# if save is unsuccessful, we render 'new' with errors
   		end
