afac514672706bea65a68e454dd02b2788403ad1
leslie kimm <leslie.kimm@gmail.com>
2016-02-06 20:31:17 -0800

    Use named routes and update header & footer files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-    root 'static_pages#home'  		# sets root route to /static_pages/home --> carries out home action in static_pages_controller.rb
-    get  'static_pages/about'		# maps requests for URL /static_pages/about to about action in static_pages_controller.rb
-    get  'static_pages/contact'
-    get  'static_pages/help'
+    root             'static_pages#home'  		# sets root route to /static_pages/home --> carries out home action in static_pages_controller.rb
+    # get  'static_pages/about'					# maps requests for URL /static_pages/about to about action in static_pages_controller.rb
+    get 'about'   => 'static_pages#about'
+    get 'contact' => 'static_pages#contact'
+    get 'help'    => 'static_pages#help'
 end
