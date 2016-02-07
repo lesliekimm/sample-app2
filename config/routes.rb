0ddb28c1ec7001b1f56d0e0895d293aa7f764bd1
leslie kimm <leslie.kimm@gmail.com>
2016-02-06 17:55:12 -0800

    Add contact page (solve exercise 3.2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-    root 'static_pages#home'  	# sets root route to /static_pages/home --> carries out home action in static_pages_controller.rb
-    get  'static_pages/about'	# maps requests for URL /static_pages/about to about action in static_pages_controller.rb
+    root 'static_pages#home'  		# sets root route to /static_pages/home --> carries out home action in static_pages_controller.rb
+    get  'static_pages/about'		# maps requests for URL /static_pages/about to about action in static_pages_controller.rb
+    get  'static_pages/contact'
     get  'static_pages/help'
 end
