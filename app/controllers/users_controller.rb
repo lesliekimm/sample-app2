86cac362fa2a7232a0a5d0e48e46ee36e208828e
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 09:59:09 -0800

    Use strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,21 @@ class UsersController < ApplicationController
   	end
 
   	def create
+  		@user = User.new(user_params)		# use aux method defined in private to require user and permit only name, email & pws
+  		if @user.save
+  			# handle successful save
+  		else
+  			render 'new'					# if save is unsuccessful, we render 'new' with errors
+  		end
   	end
 
   	def show
   		@user = User.find(params[:id])		# technically, params[:id] returns a string but find knows to convert to int
   		# debugger
   	end
+
+  	private
+  		def user_params
+  			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  		end
 end
