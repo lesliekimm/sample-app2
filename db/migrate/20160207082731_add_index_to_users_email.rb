6f8bce9ea28501aa1a2760823a30c69ab1a70aa9
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 00:46:52 -0800

    Add index to email & pw digest migrations

---
+++ db/migrate/20160207082731_add_index_to_users_email.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsersEmail < ActiveRecord::Migration
+  def change
+  	add_index :users, :email, unique: true 				# uses method add_index to add index on email column in users talbe
+  end
+end
