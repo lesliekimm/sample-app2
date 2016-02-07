4cb5188842ac6c139b2514fadd9ea2871a1b454d
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 09:20:24 -0800

    User gravatar to produce profile image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+	# Returns the Gravatar for the given user.
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)			# create an id using MD5 hash of email address
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"	# create gravatar URL
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")			# return image_tag
+	end
 end
