e9f7e697934a27675a0432922336dd5f482e778c
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 14:54:01 -0800

    Change header nav if loged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,15 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
+
+	# Returns the current logged-in user (if any).
+	# Allows us to make changes to app depending on if user is logged in or not.
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# Returns true if user is looged in, false otherwise.
+  	def logged_in?
+    	!current_user.nil?
+  	end
 end
