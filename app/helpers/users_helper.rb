ba313c4bc67de3a24dcc1704a91125e94fc6cfb5
leslie kimm <leslie.kimm@gmail.com>
2016-02-08 07:39:00 -0800

    Edit gravatar_for to take options hash to control image size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 	# Returns the Gravatar for the given user.
-	def gravatar_for(user)
+	def gravatar_for(user, options= { size: 80 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)			# create an id using MD5 hash of email address
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"	# create gravatar URL
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")			# return image_tag
