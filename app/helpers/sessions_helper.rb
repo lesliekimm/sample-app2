a59f3692f2d0fad24fd30b4af64c39a77531517d
leslie kimm <leslie.kimm@gmail.com>
2016-02-07 17:38:01 -0800

    finish log in log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,7 +29,7 @@ module SessionsHelper
   	def current_user
 		if (user_id = session[:user_id])								# this is an assignment - if session of user id exists
       		@current_user ||= User.find_by(id: user_id)
-    	elsif (user_id = cookies.signed[:user_id])						# if cookies of user_id exists
+    	elsif (user_id = cookies.signed[:user_id])						# if cookies of user_id exists												
       		user = User.find_by(id: user_id)
       		if user && user.authenticated?(cookies[:remember_token])
         		log_in user
