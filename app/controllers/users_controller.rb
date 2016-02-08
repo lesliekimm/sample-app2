3cd34b9acbce5718705d14122e9795f475f7fcfc
leslie kimm <leslie.kimm@gmail.com>
2016-02-08 07:39:16 -0800

    Display al users on users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,10 @@ class UsersController < ApplicationController
         end
     end
 
+    def index
+        @users = User.all
+    end
+
   	private
   		def user_params
   			params.require(:user).permit(:name, :email, :password, :password_confirmation)
