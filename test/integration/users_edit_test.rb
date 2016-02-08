7153987f1468ac251260f1ed30cadc586d03981c
leslie kimm <leslie.kimm@gmail.com>
2016-02-08 12:37:46 -0800

    Only forwards to desired page at first log in attempt - exercise 1

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -19,6 +19,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 	test "successful edit with friendly forwarding" do
 		# debugger
 		get edit_user_path(@user)
+		assert_not_nil session[:forwarding_url]
 		log_in_as(@user)
 		assert_redirected_to edit_user_path(@user)
 		log_in_as(@user)
@@ -32,6 +33,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 									    password_confirmation: "" }
 		assert_not flash.empty?
 		assert_redirected_to @user
+		assert_nil session[:forwarding_url]
 		@user.reload
 		assert_equal name, @user.name
 		assert_equal email, @user.email
