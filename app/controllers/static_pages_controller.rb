043aac981bc07e92caef33d4e6bd9b36f05d0479
leslie kimm <leslie.kimm@gmail.com>
2016-02-09 10:33:41 -0800

    Create partials for home page to change depending on if loggedin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home			# by generating this action, we get the home page for route /static_pages/home
+  	@micropost = current_user.microposts.build if logged_in?
   end
 
   def about
