3477a566d52fc51c602dc46a8b876750b13376a6
leslie kimm <leslie.kimm@gmail.com>
2016-02-09 13:40:23 -0800

    Email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'dry-sea-38236.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
